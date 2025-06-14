afa05bd26048c553fad9779e7c94e61fe4a6fa0e
Hyun <eric0909kim@gmail.com>
2025-06-14 07:57:33 -0400

    add batchid to logentry in processor

--- src/main/java/com/example/loganalyzer/batch/LogEntryProcessor.java
+++ src/main/java/com/example/loganalyzer/batch/LogEntryProcessor.java
@@ -1,10 +1,17 @@
 package com.example.loganalyzer.batch;
 
 import com.example.loganalyzer.model.LogEntry;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-
+@Component
+@StepScope
 public class LogEntryProcessor implements ItemProcessor<LogEntry, LogEntry> {
+
+    @Value("#{jobParameters['batchId']}")
+    private String batchId;
 
     @Override
     public LogEntry process(LogEntry entry) throws Exception {
@@ -14,6 +21,7 @@ public class LogEntryProcessor implements ItemProcessor<LogEntry, LogEntry> {
         String[] names = entry.getServerName().split("\\.");
         String serverName = names[names.length - 1];
         entry.setServerName(serverName);
+        entry.setBatchId(batchId);
 
         return entry;
     }
